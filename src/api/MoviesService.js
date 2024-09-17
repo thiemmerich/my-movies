64473fd85dd374a78c6588fea17d4fb5c33f312a
Thiago <thiago.emmerich@hotmail.com>
2024-09-17 11:08:47 -0300

    Finalizando as partes 03 do modulo 01 - Criando um SPA 'Consumindo API themoviedb usando axios'

---
+++ src/api/MoviesService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_KEY = "5e9990a3e0b5af9e9424a60eafe63129";
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'));
+    }
+}
