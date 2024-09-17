e4a7ec72ff201b70705c7f9cda9256edf0a80cf7
Thiago <thiago.emmerich@hotmail.com>
2024-09-17 11:48:58 -0300

    Finalizando a parte 05 do modulo 01 - Criando um SPA 'Inserindo a nova rota detalhando o filme selecionado passando o id do filme nos parametros da rota e fazendo uma nova requisição na API'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { About } from './views/About';
 import { Home } from './views/Home';
+import { MovieDetail } from './views/MovieDetail';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/about" element={<About />} />
+          <Route path="/movie/:id" element={<MovieDetail />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
